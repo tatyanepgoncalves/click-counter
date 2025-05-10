e6bc3bdca8324fc1b3ff515f794f6cfc5a8c3c72
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2025-05-10 08:27:26 -0300

    :sparkles: feat(js): Update page button interation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,18 @@ let contador = 0
 function updateText() {
   result.textContent = contador
   returnInfo.textContent = `Você já clicou ${contador} vezes. Está treinando para ser gamer?`
+
+  if (contador === 10) {
+    returnInfo.textContent = "Você já atingiu o limite máximo de cliques!"
+    btnPrimary.classList.add("disabled")
+    btnSecundary.classList.remove("disabled")
+  } else if (contador === 0){
+    btnPrimary.classList.remove("disabled")
+    btnSecundary.classList.add("disabled")
+  } else {
+    btnPrimary.classList.remove("disabled")
+    btnSecundary.classList.remove("disabled")
+  }
 }
 
 function resetText() {
@@ -26,13 +38,21 @@ btnPrimary.addEventListener("click", () => {
  setTimeout(() => btnPrimary.classList.remove("active"), 150);
 })
 
-btnPrimary.classList.remove("active")
 
 btnSecundary.addEventListener("click", () => {
   btnSecundary.classList.add("active")
 
   contador = 0
   resetText()
+
+  if (contador === 0) {
+    resetText()
+    btnPrimary.classList.remove("disabled")
+    btnSecundary.classList.add("disabled")
+  } else if (contador >= 1) {
+    btnPrimary.classList.remove("disabled")
+    btnSecundary.classList.add("disabled")
+  }
 
   setTimeout(() => btnSecundary.classList.remove("active"), 150)
 })
